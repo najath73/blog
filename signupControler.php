a9a0c768cc279f84c703fd767008b76f3c4bf992
Lawani Imrane <48188711+naimlawani01@users.noreply.github.com>
2023-12-22 23:46:51 +0100

    Inscription

--- signupControler.php
+++ signupControler.php
@@ -13,7 +13,20 @@ if(isset($_POST['submit'])){
         $prenomError = ctype_alpha($prenom) ? "" : "Le prenom contient des caractères non-alphabétiques.\n";
         $mdpError = strlen($motDePasse1) > 8 ? "" : "Votre mot de passe doit avoir plus de 8 caractères";
         $mdpError2= ($motDePasse1 == $motDePasse2) ? "" : "Les mots de passe ne sont pas identiques";
-        $emailError = filter_var($email, FILTER_VALIDATE_EMAIL) ? "" : "L'adresse e-mail n'est pas valide";  
+        $emailError = filter_var($email, FILTER_VALIDATE_EMAIL) ? "" : "L'adresse e-mail n'est pas valide";
+        if(empty($nomError) and empty($prenomError) and empty($mdpError) and empty($mdpError2) and empty($emailError)){
+            $insert_user = $db->prepare("INSERT INTO users(first_name, last_name, email, password, creation_date) values(:first_name, :last_name, :email, :password, :creation_date) ");
+            $insert_user->execute([
+                "first_name" => $prenom,
+                "last_name" => $nom,
+                "email" => $email,
+                "password" => password_hash($motDePasse1, PASSWORD_BCRYPT), //sha1 obselèste pas recommandé
+                "creation_date" => date("Y-m-d H:i:s")
+            ]);
+
+
+            header('location: signin.php');
+        }
     }else{
         $fieldError =  "Tous les champs sont requis";
     }
