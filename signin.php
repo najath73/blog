5896c4e63fded82e979a685c57cba690ebdd682f
Lawani Imrane <48188711+naimlawani01@users.noreply.github.com>
2023-12-23 17:29:25 +0100

    Correction signin

--- signin.php
+++ signin.php
@@ -1,11 +1,23 @@
 <?php
+session_start();
 require_once("./db.php");
 require_once("./signinControler.php");
 require_once("./header.php");
 ?>
 
 <div class="container">
-
+    <?php if(!empty($error_signin)):?> 
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <small> <?= $error_signin ?></small> 
+        </div>
+        
+        <script>
+          $(".alert").alert();
+        </script>
+    <?php endif;?>
     <h1 class="display-4">Connexion</h1>
     <?php if(!empty($fieldError)):?> 
         <small id="emailHelp" class="form-text text-danger"><?= "* " . $fieldError ?> </small>
@@ -13,14 +25,14 @@ require_once("./header.php");
     <form method="POST" action="">
         <div class="form-group">
             <label for="exampleInputEmail1">Email </label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+            <input type="email" class="form-control" id="exampleInputEmail1" value="<?= $email ?? '' ?>" placeholder="Enter email" name="email">
             <?php if(!empty($emailError)):?> 
                 <small id="emailHelp" class="form-text text-danger"><?= $emailError ?> </small>
             <?php endif;?>
         </div>
         <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input name="motDePasse1"  type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <label for="exampleInputPassword">Password</label>
+            <input name="motDePasse"  type="password" value = "<?= $motDePasse ?? '' ?>" class="form-control" id="exampleInputPassword" placeholder="Password">
             <?php if(!empty($mdpError)):?> 
                 <small id="password2Help" class="form-text text-danger"><?= $mdpError ?> </small>
             <?php endif;?>
--- signinControler.php
+++ signinControler.php
@@ -4,23 +4,29 @@ if(isset($_POST['submit'])){
 
     
     $email  = $_POST['email'];
-    $motDePasse1  = $_POST['motDePasse1'];
+    $motDePasse  = $_POST['motDePasse'];
 
-    if(!empty($email) and !empty($motDePasse1) ) {
-        $mdpError = strlen($motDePasse1) > 8 ? "" : "Votre mot de passe doit avoir plus de 8 caractères";   
+    if(!empty($email) and !empty($motDePasse) ) {
+        $mdpError = strlen($motDePasse) > 8 ? "" : "Votre mot de passe doit avoir plus de 8 caractères";   
         $emailError = filter_var($email, FILTER_VALIDATE_EMAIL) ? "" : "L'adresse e-mail n'est pas valide";
-            if (empty($mdpError) and(empty($emailError))){
-                $select_user = $db->prepare("SELECT * FROM users WHERE ( email=$email and password_hash($motDePasse1, PASSWORD_BCRYPT)=$motDePasse1");
-                if($select_user->num_rows > 0) { 
-                    echo "Connection réusssi";
-                }else{echo"Utilisateur inexistant, Veuillez vous inscrire";}
 
 
-              
+        if (empty($mdpError) and(empty($emailError))){
+            $select_user = $db->prepare("SELECT * FROM users WHERE  email=:email ");
+            $select_user->execute([
+                "email" => $email
+            ]);
+            $select_user = $select_user->fetch(PDO::FETCH_ASSOC);
+            if (password_verify($motDePasse, $select_user['password'])) {
+                $_SESSION["user_id"] = $select_user['id'];
+                header('location: index.php');
+            } else {
+                $error_signin = "Adresse mail ou mot de passe incorect";
             }
+        }
         
            
-    }else{$fieldError =  "Tous les champs sont requis";
-
+    }else{
+        $fieldError =  "Tous les champs sont requis";
     }
 }
